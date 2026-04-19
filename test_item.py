2fa8a5cb2e185b37dbf148c84746f458bd22dd3d
KAMIKAZEE314 <pinguinos313@gmail.com>
2026-04-19 19:44:09 +0200

    added function to convert json_form to class object

--- test_item.py
+++ test_item.py
@@ -10,3 +10,8 @@ def test_Item_getters():
 def test_Item_json():
 	item = Item("OaK plAnk", "mInEcRAft ", 24)
 	assert item.json_form() == {"item": "oak_plank", "mod": "minecraft", "count": 24}
+
+def test_Item_from_json():
+	item = Item("OaK plAnk", "mInEcRAft ", 24)
+	json_form = {"item": "oak_plank", "mod": "minecraft", "count": 24}
+	assert Item.from_json(json_form) == item
